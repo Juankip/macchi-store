2d246cf67d88d2ec3270c3923f8f2fd9578849a5
Juan Sebastián Macchi <juasemacchi@gmail.com>
2026-06-22 17:28:25 -0300

    Agregada la extension .jsx al import de Checkout

--- macchi-store/src/App.jsx
+++ macchi-store/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/views/Inicio.jsx";
 import ItemListContainer from './components/layout/ItemListContainer';
 import ItemDetailContainer from './components/views/ItemDetailContainer';
 import Cart from './components/views/Cart';
-import Checkout from './components/views/Checkout'; // Importamos la vista nueva
+import Checkout from "./components/views/Checkout.jsx";
 
 function App() {
   return (
