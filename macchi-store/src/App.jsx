8a4be1f89e8deb8be744f62c80655aa4c4046987
Juan Sebastián Macchi <juasemacchi@gmail.com>
2026-06-22 17:22:40 -0300

    Forzando cambio de nombre a Inicio

--- macchi-store/src/App.jsx
+++ macchi-store/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Layout from './components/layout/Layout';
-import Home from "./components/views/Home.jsx";
+import Home from "./components/views/Inicio.jsx";
 import ItemListContainer from './components/layout/ItemListContainer';
 import ItemDetailContainer from './components/views/ItemDetailContainer';
 import Cart from './components/views/Cart';
