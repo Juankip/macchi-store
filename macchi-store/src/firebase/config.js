891c2d7a0bc203742b467c206c3cd87213f7b59a
Juan Sebastián Macchi <juasemacchi@gmail.com>
2026-06-29 20:31:42 -0300

    Protegiendo claves

--- macchi-store/src/firebase/config.js
+++ macchi-store/src/firebase/config.js
@@ -2,16 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBm-rk0PSwh0BrbMiafJKXFiR7WiqAXe10",
-  authDomain: "gen-rockero.firebaseapp.com",
-  projectId: "gen-rockero",
-  storageBucket: "gen-rockero.firebasestorage.app",
-  messagingSenderId: "935248961974",
-  appId: "1:935248961974:web:6113016e95e22bce742117"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializamos la base de datos y la exportamos
 export const db = getFirestore(app);
